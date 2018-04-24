062aa0496e4730be07eb5e2558e6f32358fe0c89
Divyesh Chitroda <div5yesh@gmail.com>
2018-04-25 01:13:21 +0530

    add gesture Recogniser to launch apps
    WIP: code cleanup

--- src/Launch.java
+++ src/Launch.java
@@ -1,8 +1,4 @@
-import java.awt.AWTException;
 import java.awt.CheckboxMenuItem;
-import java.awt.Frame;
-import java.awt.Graphics;
-import java.awt.Menu;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
 import java.awt.SystemTray;
@@ -11,62 +7,48 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.File;
-import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
 
-public class SnapStart extends Frame
+public class Launch 
 {
-	private int state;
-	static TrayIcon trayIcon;
-	public static void main(String s[]) throws Exception
+	private static int state;
+	private static TrayIcon trayIcon;
+	public static void threads()
 	{
-		new SnapStart("SnapGest");
-	}	
-	public SnapStart(String s) throws IOException, AWTException
+		Thread snapThread=new SnapThread();
+		snapThread.start();
+	}
+	public static void toTray(final SnapGUI gui) throws Exception
 	{
-		super(s);
-		setSize(1024,768);
-		setLocation(100, 0);
-		
-	//	setUndecorated(true);
-//		JButton min=new JButton(new ImageIcon("bulb.gif"));
-//		min.setBounds(0, 0, 30, 12);
-//		setLayout(null);
-//		add(min);
-	
 		final PopupMenu popup = new PopupMenu();
         trayIcon = new TrayIcon(ImageIO.read(new File("SnapGest.png")));
         final SystemTray tray = SystemTray.getSystemTray();
-        
+ //----------------------------------------------------------------------------------       
         final CheckboxMenuItem show = new CheckboxMenuItem("Show");
         MenuItem about = new MenuItem("About");
         MenuItem exit = new MenuItem("Exit");
-        
+ //---------------------------------------------------------------------------------       
         popup.add(show);
         popup.addSeparator();
         popup.add(about);
         popup.addSeparator();
         popup.add(exit);
-        
+//---------------------------------------------------------------------------------------      
         trayIcon.setPopupMenu(popup);
         tray.add(trayIcon);
         trayIcon.setImageAutoSize(true);
         trayIcon.setToolTip("SnapGest");
         trayIcon.displayMessage("SnapGest","Your gesture suite is running.", TrayIcon.MessageType.INFO);
-        
-        setVisible(false);
-      // SnapThread.g=getGraphics();
-        SnapThread.state=ItemEvent.DESELECTED;
+//----------------------------------------------------------------------------------------------      
+        gui.setVisible(true);
+        gui.setLocation(100, 0);
+        SnapThread.g=gui.getGraphics();
+        SnapThread.state=ItemEvent.SELECTED;
+//---------------------------------------------------------------
         show.addItemListener(new ItemListener()
         {
         	public void itemStateChanged(ItemEvent e)
@@ -74,18 +56,19 @@ public class SnapStart extends Frame
         		state=e.getStateChange();
         		if(state==ItemEvent.SELECTED)
         		{
-        			setVisible(true);
-        			SnapThread.g=getGraphics();
+        			gui.setVisible(true);
+        			gui.setLocation(100, 0);
+        			SnapThread.g=gui.getGraphics();
         			SnapThread.state=ItemEvent.SELECTED;
         		}
         		if(state==ItemEvent.DESELECTED)
         		{
-        			setVisible(false);
+        			gui.setVisible(false);
         			SnapThread.state=ItemEvent.DESELECTED;
         		}
         	}
         });
-        
+//---------------------------------------------------------------
         about.addActionListener(new ActionListener()
         {
         	public void actionPerformed(ActionEvent e)
@@ -93,6 +76,7 @@ public class SnapStart extends Frame
         		JOptionPane.showMessageDialog(null,"Created by:\nDivyesh\nJigar");
         	}
         });
+//----------------------------------------------------------------
         exit.addActionListener(new ActionListener()
         {
         	public void actionPerformed(ActionEvent e)
@@ -100,33 +84,6 @@ public class SnapStart extends Frame
         		System.exit(0);
         	}
         });
-   
-		
-		Thread t1=new SnapThread();
-		t1.start();
-		
-//		min.addActionListener(new ActionListener()
-//		{
-//			public void actionPerformed(ActionEvent e) 
-//			{	
-//				setVisible(false);
-//    			SnapThread.state=ItemEvent.DESELECTED;
-//    			show.setState(false);
-//			}
-//		});
-		
-		addWindowListener(new WindowAdapter()
-		{
-			public void windowClosing(WindowEvent e)
-			{
-				System.exit(0);
-			}
-			public void windowIconified(WindowEvent e)
-			{
-				setVisible(false);
-    			SnapThread.state=ItemEvent.DESELECTED;
-    			show.setState(false);
-			}
-		});
+//-----------------------------------------------------------------
 	}
 }
